762edc8f016b0edc3df1d8a3e4940fa8daaf108f
Elena <elena.tim@bk.ru>
2022-08-08 15:54:10 +0300

    ответ29

---
+++ otvet29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+// Console.Write("Введите число N: ");
+
+int [] array = GitArray(8);
+Console.Write($"[{String.Join(",", array)}]");
+int[] GitArray(int size)
+{
+    int[] result = new int[size];
+    for (int i=0; i< result.Length; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
